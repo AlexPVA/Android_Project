8e9b9752b9d01328c707d8e4e2cb30dc4a79a28e
alexpetersvanaalst <alex.petersvanaalst@mail.utoronto.ca>
2018-11-24 12:48:08 -0500

    Refactor

--- Phase2/GameCentre/app/src/test/java/csc207phase2/gamecentre/MinesweeperManagerUnitTest.java
+++ Phase2/GameCentre/app/src/test/java/csc207phase2/gamecentre/MinesweeperManagerUnitTest.java
@@ -7,18 +7,18 @@ import static org.junit.Assert.*;
 public class MinesweeperManagerUnitTest {
     @Test
     public void puzzleSolved_isCorrect() {
-        MinesweeperManager m = new MinesweeperMoard(9, 9);
+        MinesweeperManager m = new MinesweeperManager(9, 9);
         m.touchMove(1);
         assertEquals(m.puzzleSolved(), false);
     }
     @Test
     public void isValidTapTrue_isCorrect() {
-        MinesweeperManager m = new MinesweeperMoard(9, 9);
+        MinesweeperManager m = new MinesweeperManager(9, 9);
         assertEquals(m.isValidTap(1), True);
     }
     @Test
     public void isValidTapFalse_isCorrect() {
-        MinesweeperManager m = new MinesweeperMoard(9, 9);
+        MinesweeperManager m = new MinesweeperManager(9, 9);
         m.touchMove(1);
         assertEquals(m.isValidTap(1), True);
     }
