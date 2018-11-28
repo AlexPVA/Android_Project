f2da7e173af61ee55283932fc18e1f4bba0b2cb7
Justin Phung <justin.phung@mail.utoronto.ca>
2018-11-28 18:32:21 -0500

    fixed SlidingTilesManagerUnitTest.java to be able to run, and edited the code so it is more readable.

--- Phase2/GameCentre/app/src/test/java/csc207phase2/gamecentre/SlidingTilesManagerUnitTest.java
+++ Phase2/GameCentre/app/src/test/java/csc207phase2/gamecentre/SlidingTilesManagerUnitTest.java
@@ -18,26 +18,49 @@ public class SlidingTilesManagerUnitTest {
         SlidingTilesManager m = new SlidingTilesManager(3,3);
         boolean equal = false;
         if(m.getBoard().getTile(0,1).getId()==25 ||
-                m.getBoard().getTile(1,0).getId()==25){equal = true;}
+                m.getBoard().getTile(1,0).getId()==25){
+            equal = true;
+        }
         assertEquals(m.isValidTap(0), equal);
     }
     @Test
     public void touchMove() {
-        int position=0;int c=0;
+        int position=0;
+        int c=0;
         SlidingTilesManager m = new SlidingTilesManager(3,3);
-        for(int row=0;row<3;row++){for(int col=0;col<3;col++)
-        {if(m.getBoard().getTile(row,col).getId()==25)position=row*3+col;c=col; }}
-        if(c!=2){m.touchMove(position+1);}else{m.touchMove(position-1);}
+        for(int row=0;row<3;row++){
+            for(int col=0;col<3;col++) {
+                if(m.getBoard().getTile(row,col).getId()==25)position=row*3+col;c=col;
+            }
+        }
+        if(c!=2){
+            m.touchMove(position+1);
+        } else {
+            m.touchMove(position-1);
+        }
         assertTrue(m.isValidTap(position));
     }
 
     @Test
     public void undo_isCorrect() {
-        int position=0;int c=0; int r=0;
+        int position=0;
+        int c=0;
+        int r=0;
         SlidingTilesManager m = new SlidingTilesManager(3,3);
-        for(int row=0;row<3;row++){for(int col=0;col<3;col++)
-        {if(m.getBoard().getTile(row,col).getId()==25)position=row*3+col;r=row;c=col; }}
-        if(c!=2){m.touchMove(position+1);}else{m.touchMove(position-1);}
+        for(int row=0;row<3;row++){
+            for(int col=0;col<3;col++) {
+                if(m.getBoard().getTile(row,col).getId()==25) {
+                    position = row * 3 + col;
+                    r = row;
+                    c = col;
+                }
+            }
+        }
+        if(c!=2){
+            m.touchMove(position+1);
+        } else {
+            m.touchMove(position-1);
+        }
         m.undo(m.stepSaver.undo());
         assertEquals(m.getBoard().getTile(r,c).getId(), 25);
     }
